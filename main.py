265f9ad061f3a1b2c0397ee1362025aff1762d9f
Dmitry Ryzhenkov <cuicui@arch>
2024-09-13 13:10:52 +0200

    he añadido el dataset de iris

--- main.py
+++ main.py
@@ -1,7 +1,10 @@
 import streamlit as st
+import seaborn as sns
 
 def main():
     st.title("Hola mundo")
+    df = sns.load_dataset("iris")
+    st.dataframe(df)
 
 if __name__ == "__main__":
     main()
